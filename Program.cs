2770d1e392481caeb16c6feb7b10dfd26eca403e
Sarang Gajbhare <139471163+SarangGajbhare@users.noreply.github.com>
2025-04-21 22:56:38 +0530

    Random_number beginner Project

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace RandomNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           int name = (int)City.Nagpur;
+
+            Console.WriteLine((int)name);
+
+           
+         
+        }
+        enum  City
+        {
+            Tokyo,
+            LA,
+            Mumbai,
+            Sydney,
+            Nagpur,
+            Nanded,
+            California
+
+
+        }
+
+    }
+}
+
